6df4e1111d94aafbbf4f8073e73a7bc39b391c2e
justinthomas0789 <justinthomas0789@gmail.com>
2020-03-19 08:48:07 +0530

    Update Table.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,37 +1,43 @@
 import React from 'react';
-import { Table } from 'react-bootstrap';
+import { Table, Alert } from 'react-bootstrap';
 
 const EmployeeTable = (props) => {
     console.log(props.users)
     return (
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Age</th>
-                <th>Department</th>
-                <th>Blood Group</th>
-                <th>Address</th>
-                <th>Number</th>
-                </tr>
-            </thead>
-            <tbody>
-                {props.users.map((item,key)=>{
-                    return(
-                        <tr key={key}>
-                            <td>{item.id}</td>
-                            <td>{item.name}</td>
-                            <td>{item.age}</td>
-                            <td>{item.department}</td>
-                            <td>{item.bloodg}</td>
-                            <td>{item.address}</td>
-                            <td>{item.phone}</td>
+        <>
+            {(props.users.length>0)? 
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Age</th>
+                        <th>Department</th>
+                        <th>Blood Group</th>
+                        <th>Address</th>
+                        <th>Number</th>
                         </tr>
-                    );
-                })}
-            </tbody>
-        </Table>
+                    </thead>
+                    <tbody>
+                        {props.users.map((item,key)=>{
+                            return(
+                                <tr key={key}>
+                                    <td>{item.id}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.age}</td>
+                                    <td>{item.department}</td>
+                                    <td>{item.bloodg}</td>
+                                    <td>{item.address}</td>
+                                    <td>{item.phone}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </Table>
+                :
+                <Alert><h1>No data to display</h1></Alert>
+            }
+        </>
     );
 };
 

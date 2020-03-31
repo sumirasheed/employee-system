0c35ea8e473e719c16c193036e8b57b2f76ca1f8
sumirasheed <sasumi4@gmail.com>
2020-03-31 17:17:48 +0530

    changes

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,40 +1,114 @@
-import React from 'react';
-import { Table } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Table, Button, ButtonGroup, Alert } from 'react-bootstrap';
 
 const EmployeeTable = (props) => {
-    console.log(props.userData)
+    let [empId, setEmpid] = useState(null);
+
+    //Function to edit and save employee data from table
+    let updateUser = (e,id) => {
+        let action = e.target.getAttribute("data-action");
+        if(action === 'edit'){
+            e.target.setAttribute("data-action","update");
+            e.target.innerHTML="Save";
+            setEmpid(id);
+        } else{
+            e.target.setAttribute("data-action","edit");
+            e.target.innerHTML="Edit";
+            setEmpid(null);
+            let parent = document.getElementById("emp"+id);
+            let inputElements = parent.querySelectorAll("input, textarea");
+            let user={
+                id:id+1,
+                name: '',
+                age: '',
+                department: '',
+                bloodg:'',
+                address: [],
+                phone: []
+            };
+            Array.prototype.map.call(inputElements,(element,key)=>{
+                if(element.name==="phone"||element.name==="address"){
+                    user[element.name].push(element.value)
+                } else{
+                    user[element.name]=element.value
+                }
+            })      
+            props.updateUser(id,user);
+        }
+    }
+
+    //To delete from table
+    let deleteUser = (e,id) => {
+        props.deleteUser(id);
+    }
     return (
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                <th>#</th>
-                <th>Name</th>
-                <th>Age</th>
-                <th>Department</th>
-                <th>Blood Group</th>
-                <th>Address</th>
-                <th>Number</th>
-                </tr>
-            </thead>
-            <tbody>
-                {/* {props.showField && props.userData && props.userData.map((item,key)=>{
-                    return(
-                    <tr>
-                        <td>{key}</td>
-                        <td>{item.name}</td>
-                        <td>{item.age}</td>
-                        <td>{item.department}</td>
-                        <td>{item.bloodg}</td>
-                        <td>{item.address}</td>
-                        <td>{item.phone}</td>
-                    </tr>
-                    );
-                })} */}
-               
-                
-                
-            </tbody>
-        </Table>
+        <>
+            {(props.users.length>0)? 
+                <Table striped bordered hover responsive>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Age</th>
+                            <th>Department</th>
+                            <th>Blood Group</th>
+                            <th>Number</th>
+                            <th>Address</th>
+                            <th>Update</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {props.users.map((item,key)=>{
+                            console.log(item.phone);
+                            return(
+                                <tr key={key} id={"emp"+key} className={(empId===key)?"edit":"update"}>
+                                    <td><p>{item.id}</p></td>
+                                    <td>
+                                        <p>{item.name}</p>
+                                        <input type="text" name='name' defaultValue={item.name}/>
+                                    </td>
+                                    <td>
+                                        <p>{item.age}</p>
+                                        <input type="text" name='age' defaultValue={item.age}/>
+                                    </td>
+                                    <td>
+                                        <p>{item.department}</p>
+                                        <input type="text" name='department' defaultValue={item.department}/>    
+                                    </td>
+                                    <td>
+                                        <p>{item.bloodg}</p>
+                                        <input type="text" name='bloodg' defaultValue={item.bloodg}/>
+                                    </td>
+                                    <td>{
+                                        item.phone.map((item,index)=>{
+                                            
+                                            return(
+                                              <div key={index} data-id={index}><p>{item}</p><input type="text" defaultValue={item} data-id={index} name='phone'/></div>  
+                                            )
+                                        })
+                                    }</td>
+                                    <td>{
+                                        item.address.map((item,index)=>{
+                                            return(
+                                                <div key={index} data-id={index}><p>{item}</p><input type="text" defaultValue={item} data-id={index}  name='address'/></div> 
+                                            )
+                                        })    
+                                    }</td>
+                                    <td>
+                                        <ButtonGroup>
+                                            <Button variant="primary" data-action="edit" data-id={key} onClick={(e)=>updateUser(e,key)}>Edit</Button>
+                                            <Button variant="danger" data-id={key} onClick={(e)=>deleteUser(e,key)}>Delete</Button>
+                                        </ButtonGroup>
+                                    </td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </Table>
+                :
+                <Alert><h1>No data to display</h1></Alert>
+            }
+        </>
     );
 };
 
--- src/containers/Employee.js
+++ src/containers/Employee.js
@@ -2,66 +2,139 @@ import React, { Component } from 'react';
 import { Container,Row, Col } from 'react-bootstrap';
 import EmployeeForm from '../components/Form';
 import EmployeeTable from '../components/Table';
+
 class Employee extends Component {
-    userData=[];
+    userData;
+    arrayValues=[];
     constructor(props) {
         super(props);
         this.state = {
+            id:null,
             name: '',
             age: '',
             department: '',
             bloodg:'',
-            address:'',
-            phone:'',
-            showField:false
+            address: [],
+            phone: [],
+            users:[],
+            phoneCount: 1,
+            addressCount: 1
         }
     }
 
-     // React Life Cycle
-     componentDidMount() {
-        this.userData = JSON.parse(localStorage.getItem('document'));
+    // React Life Cycle
+    componentDidMount() {
+        this.getDataFromLocal(); 
+    }
 
-        if (localStorage.getItem('document')) {
+    //Function to get data from Local Storage
+    getDataFromLocal = () => {
+        this.userData = JSON.parse(localStorage.getItem('users'));
+        if (this.userData) {
             this.setState({
-                name: this.userData.name,
-                age: this.userData.age,
-                department: this.userData.department,
-                bloodg:this.userData.bloodg,
-                address: this.userData.address,
-                phone: this.userData.phone,
-            })
-        } else {
+                users: this.userData,
+            });
+        } 
+    }
+
+    //Function to save data to Local Storage
+    setDataToLocal = () =>{
+        localStorage.setItem('users',JSON.stringify(this.state.users)) 
+    }
+
+    //Function to add multiple address and phone number dynamically
+    onLinkClick = (e,field) =>{
+        e.preventDefault();
+        switch(field){
+            case "phone":
+                this.setState({
+                    phoneCount:this.state.phoneCount+1
+                })
+                break;
+            case "address":
+                this.setState({
+                    addressCount:this.state.addressCount+1
+                })
+                break;
+            default:
+                this.setState({
+                    addressCount:this.state.addressCount+1
+                })
+        }
+        
+    }
+
+    //Function to submit Employee Form
+    submitForm(e) {
+        e.preventDefault();
+        this.getDataFromLocal();
+        let users = this.state.users
+        let empty = [];
+        let user ={
+            id:users.length+1,
+            name: this.state.name,
+            age: this.state.age,
+            department: this.state.department,
+            bloodg:this.state.bloodg,
+            address:this.state.address,
+            phone:this.state.phone,
+        }
+        if(user.name !== ''){
+            users.push(user);
             this.setState({
+                users: users,
                 name: '',
                 age: '',
                 department: '',
                 bloodg:'',
-                address: '',
-                phone: ''
-            })
+                address: empty,
+                phone: empty
+
+            },this.setDataToLocal);
+        } else{
+            alert('Please fill name')
         }
+         
     }
 
-    submitForm(e) {
-        e.preventDefault()
-        localStorage.setItem('document',JSON.stringify(this.state));
-        this.setState({
-             name: '',
-                age: '',
-                department: '',
-                bloodg:'',
-                address: '',
-                phone: '',
-                showField:true
-        })
-    }
-
+    //Function on change event of input fields
     handleChange = (event) => {
+        console.log(event.target.value)
+        if(event.target.name === "phone"||event.target.name==="address"){
+            let aId = event.target.getAttribute("data-id");
+            this.arrayValues = this.state[event.target.name];
+            this.arrayValues[aId] = event.target.value;
+            this.setState({
+                [event.target.name]: this.arrayValues
+            });
+        } else{
+            this.setState({
+                [event.target.name]: event.target.value
+            });
+        }
+        
+    } 
+
+    //Function to delete employee from Local Storage
+    deleteUser = (id) =>{
+        let users = this.state.users.slice(0, id).concat(this.state.users.slice(id + 1, this.state.users.length));
         this.setState({
-            [event.target.name]: event.target.value
-        });
+            users: users,
+        },this.setDataToLocal);
     }
-    
+
+    //Function to update employee data in the Local Storage
+    updateUser = (id,user) =>{
+        let users = this.state.users;
+        if(user.name !== ''){
+            users[id]=user;
+            this.setState({
+                users: users,
+            },this.setDataToLocal);
+        } else{
+            alert('Please fill name')
+        }
+    }  
 
     render() {
         return (
@@ -74,10 +147,10 @@ class Employee extends Component {
                                 <EmployeeForm submitForm={this.submitForm.bind(this)} handleChange={this.handleChange} 
                                 name={this.state.name} age={this.state.age} 
                                 department={this.state.department} bloodg={this.state.bloodg} 
-                                address={this.state.address} phone={this.state.phone} />
+                                address={this.state.address} phone={this.state.phone} onLinkClick={this.onLinkClick} phoneCount={this.state.phoneCount} addressCount={this.state.addressCount}/>
                             </Col>
                             <Col md={8}>
-                                <EmployeeTable userData={this.userData} showField={this.state.showField}/> 
+                                <EmployeeTable users={this.state.users} updateUser={(id,user)=>this.updateUser(id,user)}  deleteUser={(id)=>this.deleteUser(id)} handleChange={this.handleChange}/> 
                             </Col> 
                         </Row>
                     </div>
